2eb5031f32bb5f6e09cef3222d396024f4e947df
Muhammad Zahid <muhammadali775@gmail.com>
2022-02-19 00:58:34 -0500

    completed question 2

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # big-o-1
 
-the world is simply not enough
-I asid the workd is simply not enough
+Frequency Counter -------------------------------
+//Write a function called same, which accepts two arrays. The function should return true if every value in the array has its corresponding
+//value squared in the second array. The frequency of values must be the same. 
+
+
+//Given two strings, write a function to determine if the second string is an anagram of the first. An anagram is a word, phrase, or name formed by rearranging the letters of
+another, such as cinema, formed from iceman.
+
--- frequencyCounter.js
+++ frequencyCounter.js
@@ -1,19 +1,80 @@
-function samefunction same(arr1, arr2){
+//Write a function called same, which accepts two arrays. The function should return true if every value in the array has its corresponding
+//value squared in the second array. The frequency of values must be the same. 
+
+//naive solution
+function same(arr1, arr2){
     if (arr1.length !== arr2.length){
-      console.log(false)
+    //   console.log(false)
       return false
     }
     
    for (let i = 0; i < arr2.length; i++){
      index = arr2.indexOf(arr1[i]** 2)
      if(index == -1){
-       console.log(false)
+    //    console.log(false)
        return false
      }
      arr2.splice(index, 1)
    }
-  console.log(true)
+//   console.log(true)
     return true
   }
   
   same([1,2,3],[1,4,9,9])
+
+//SOLID SOLUTION
+function same(arr1, arr2){
+    if(arr1.length !== arr2.length){
+        return false;
+    }
+    let frequencyCounter1 = {}
+    let frequencyCounter2 = {}
+    for(let val of arr1){
+        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+    }
+    for(let val of arr2){
+        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1        
+    }
+    console.log(frequencyCounter1);
+    console.log(frequencyCounter2);
+    for(let key in frequencyCounter1){
+        if(!(key ** 2 in frequencyCounter2)){
+            return false
+        }
+        if(frequencyCounter2[key ** 2] !== frequencyCounter1[key]){
+            return false
+        }
+    }
+    return true
+}
+
+same([1,2,3,2,5], [9,1,4,4,11])
+
+
+//QUESTION 2
+
+function same(str1, str2){
+    if(str1.length!==str2.length){
+      console.log(false)
+    }
+    if(str1==str2){
+      console.log("these are exactly the same")
+    }
+  let obj1 ={}
+  let obj2 ={}
+    for (let val of str1){
+      obj1[val]= (obj1[val] || 0) + 1 
+    }
+    for (let val of str2){
+      obj2[val]= (obj2[val] || 0) + 1
+    }
+    for (let key in obj1){
+      if(obj2[key]!== obj1[key]){
+      console.log(`value does not exist in object 2 ${key}`)
+      return false
+      }
+    }
+    console.log(true) 
+  }
+  
+  same("hits", "hits")
