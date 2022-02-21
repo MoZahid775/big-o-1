d6168badadef8126b8547886c5bd173ac5022703
Muhammad Zahid <muhammadali775@gmail.com>
2022-02-21 01:49:59 -0500

    attempt at unique values

--- BmultiplePointers.js
+++ BmultiplePointers.js
@@ -1,26 +1,30 @@
+// Write a function called sumZero which accepts a sorted array of integers. 
+// The function should find the first pair where the sum is 0. Return an array that 
+// includes both values that sum to zero or undefined if a pair does not exist. 
+
+
+
 function sumZero(arr){
-    let left = 0;
-    let right = arr.length - 1;
-    while (left < right){
-        let sum = arr[left] + arr[right];
-        if (sum === 0){
-            return [arr[left], arr[right]];
-        } else if (sum > 0){
-            right--;
-        } else {
-            left++;
-        }
-    }
-}
-
-
-firstP = 0
-secondP = 1
-  (let i =0;          : )
-  if (arr.length==0){
-      return 0
-  }
-
+    let left = 0
+    let right = arr.length -1
+     while(left<right){
+       let sum = arr[left] + arr[right]
+       if(sum === 0 ){
+         console.log(arr[left], arr[right])
+         return arr[left], arr[right]
+       }else if(sum > 0){
+       right = right-= 1
+       }else {
+         left++
+       }
+       
+   
+       
+     }
+     console.log("the results are inconclusive")
+   }
+   
+   sumZero([-5,-2,-1,0,7,10])
 
 
   //attempt at count unique values
