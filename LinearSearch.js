69f3a01550b7ca3ace575b1c55f121938ef95b15
Muhammad Zahid <muhammadali775@gmail.com>
2022-02-21 23:00:30 -0500

    linear search without index

---
+++ LinearSearch.js
@@ -0,0 +1,19 @@
+//WRITE A FUNCTION CALLED LINEARSEARCH WHICH ASSPETS AN ARRAY AND A VALUE, AND RETURNS THE INDEX AT WHICH THE VALUE EXISTS. IF TYHE VALUE DOES NOT EXIST IN THE ARRAY, RETURN -1
+
+
+
+
+function linearSearch(arr, val){
+
+    let index = arr.indexOf(val)
+  
+  
+   return index
+    
+   // index > -1 ? return index : console.log(false) 
+    
+    
+  }
+  
+
+  //attempt without using the indexOF
